176ad9f61a100d51ac78c442e56bf722455b2b8e
Ebou Jobe <ebou.jobe@mail.mcgill.ca>
2018-01-20 04:47:19 -0500

    copy the distance validation check from PCController to BangBangController

--- src/ca/mcgill/ecse211/wallfollowing/BangBangController.java
+++ src/ca/mcgill/ecse211/wallfollowing/BangBangController.java
@@ -9,6 +9,8 @@ public class BangBangController implements UltrasonicController {
   private final int motorLow;
   private final int motorHigh;
   private int distance;
+  private int filterControl;
+  private static final int FILTER_OUT = 20;
   
 
   public BangBangController(int bandCenter, int bandwidth, int motorLow, int motorHigh) {
@@ -17,6 +19,7 @@ public class BangBangController implements UltrasonicController {
     this.bandwidth = bandwidth;
     this.motorLow = motorLow;
     this.motorHigh = motorHigh;
+    this.filterControl = 0;
     WallFollowingLab.leftMotor.setSpeed(motorHigh); // Start robot moving forward
     WallFollowingLab.rightMotor.setSpeed(motorHigh);
     WallFollowingLab.leftMotor.forward();
@@ -28,13 +31,26 @@ public class BangBangController implements UltrasonicController {
     this.distance = distance;
     
     
-
-    
     //The implementation instruction is  from the slides on mycources.
     
+    if (distance >= 255 && filterControl < FILTER_OUT) {
+        // bad value, do not set the distance var, however do increment the
+        // filter value
+        filterControl++;
+      } else if (distance >= 255) {
+        // We have repeated large values, so there must actually be nothing
+        // there: leave the distance alone
+        this.distance = distance;
+      } else {
+        // distance went below 255: reset filter and leave
+        // distance alone.
+        filterControl = 0;
+        this.distance = distance;
+      }
+    
     int error = this.distance - this.bandCenter;	//error in cm
-	if(Math.abs(error) <= this.bandwidth)	//abs is less than the threshold
-	{										//robot is in the right direction //just keep moving
+	if(Math.abs(error) <= this.bandwidth)	    //abs is less than the threshold
+	{										    //robot is in the right direction //just keep moving
 		WallFollowingLab.leftMotor.setSpeed(this.motorHigh);
 		WallFollowingLab.rightMotor.setSpeed(this.motorHigh);
 		WallFollowingLab.leftMotor.forward();
